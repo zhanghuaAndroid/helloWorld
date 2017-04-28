91e521f942d5077d83261b793c83621c79cf3f42
zhanghua <1007512140@163.com>
2017-04-28 12:14:18 +0800

    加入了一行注释

--- app/src/main/java/com/jiuselu/helloworld/MainActivity.java
+++ app/src/main/java/com/jiuselu/helloworld/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //一个全新的开始
         setContentView(R.layout.activity_main);
     }
 }
